a9934677ea365b75d82f0a97f39a76c400bb0756
Ramin Barati <rekino@gmail.com>
2025-07-12 19:07:30 +0200

    remove hallucinated navigation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,7 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/constitution" className="[&.active]:font-bold">
-          Constitution
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-    </>
+    <Outlet />
   ),
 })
