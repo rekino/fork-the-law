1a62889acbc4f9164de720cc30fce7c1f103a085
Ramin Barati <rekino@gmail.com>
2025-07-13 15:19:46 +0200

    add navbar to the layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import Navbar from "@/components/partials/navbar"
 
 export const Route = createRootRoute({
   component: () => (
-    <Outlet />
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   ),
 })
