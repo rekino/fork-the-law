3687477d2c8650a8fd2a6ecc131f29fabb3d827c
Ramin Barati <rekino@gmail.com>
2025-07-14 10:15:50 +0200

    add tests for routes

--- src/routes.test.tsx
+++ src/routes.test.tsx
@@ -13,26 +13,45 @@ const createTestRouter = (initialUrl: string) => {
 }
 
 describe('Router', () => {
-  it('should render the home page', async () => {
-    const router = createTestRouter('/')
+  it('should render the home page for the en locale', async () => {
+    const router = createTestRouter('/en/')
     render(<RouterProvider router={router} />)
-    await screen.findByText('Welcome Home!')
-    expect(screen.getByText('Welcome Home!')).toBeInTheDocument()
+    await screen.findByText('Reimagine the Constitution')
+    expect(screen.getByText('Reimagine the Constitution')).toBeInTheDocument()
   })
 
-  it('should render the constitution page', async () => {
-    const router = createTestRouter('/constitution')
+  it('should render the home page for the fa locale', async () => {
+    const router = createTestRouter('/fa/')
     render(<RouterProvider router={router} />)
-    await screen.findByRole('heading', { name: /constitution/i })
-    expect(
-      screen.getByRole('heading', { name: /constitution/i }),
-    ).toBeInTheDocument()
+    await screen.findByText('Reimagine the Constitution')
+    expect(screen.getByText('Reimagine the Constitution')).toBeInTheDocument()
+  })
+
+  it('should render the constitution page for the en locale', async () => {
+    const router = createTestRouter('/en/constitution')
+    render(<RouterProvider router={router} />)
+    await screen.findByText('Constitution')
+    expect(screen.getByText('Constitution')).toBeInTheDocument()
+  })
+
+  it('should render the constitution page for the fa locale', async () => {
+    const router = createTestRouter('/fa/constitution')
+    render(<RouterProvider router={router} />)
+    await screen.findByText('Constitution')
+    expect(screen.getByText('Constitution')).toBeInTheDocument()
   })
 
   it('should render a not found page for an unknown route', async () => {
-    const router = createTestRouter('/some-random-route')
+    const router = createTestRouter('/en/some-random-route')
     render(<RouterProvider router={router} />)
     await screen.findByText('Not Found')
     expect(screen.getByText('Not Found')).toBeInTheDocument()
+  })
+
+  it('should navigate to english index page on root', async () => {
+    const router = createTestRouter('/')
+    render(<RouterProvider router={router} />)
+    await screen.findByText('Reimagine the Constitution')
+    expect(screen.getByText('Reimagine the Constitution')).toBeInTheDocument()
   })
 })
